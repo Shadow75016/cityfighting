234ab452be00454ea75f023c57f66d5cd658726d
Bastien <167779241+Shadow75016@users.noreply.github.com>
2025-04-25 18:31:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -332,6 +332,7 @@ with col2:
 # === Filtre global pour les POIs (valable pour les deux villes) ===
 
 
+
 data_ville1 = get_ville_data(ville1)
 data_ville2 = get_ville_data(ville2)
 
@@ -343,17 +344,14 @@ if data_ville1 and data_ville2:
         default=[]
     )
 
+    types_disponibles = ["école", "hôpitaux", "parc", "gare"]
+
     col1, col2 = st.columns(2)
 
     for col, data in zip([col1, col2], [data_ville1, data_ville2]):
         with col:
             st.markdown(f"""
                 <div class='card'>
-types_selectionnes = st.multiselect(
-    "📍 Filtrer les types de points d’intérêt à afficher :", 
-    options=types_disponibles, 
-    default=[]
-
                     <h3><strong>📍{data['nom']}</strong></h3>
                     <p><strong>Population :</strong> {data['population']} habitants</p>
                     <p><strong>Superficie :</strong> {data['superficie_km2']} km²</p>
